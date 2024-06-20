4d0d9ddedcdc557663e4d1ee3b636673fc886725
Mayurijire1525 <Mayurijire441@gmail.com>
2024-06-20 19:43:07 +0530

    Daily code

--- Bubble_Sort/src/sorting/SortingMain.java
+++ Bubble_Sort/src/sorting/SortingMain.java
@@ -26,11 +26,27 @@ public class SortingMain {
 			}
 		}
 	}
+	public static void moreimpbubbleSort(int[] arr) {
+		for(int i=0; i<arr.length-1; i++) {
+			boolean swapflag=false;
+			for(int j=0;j<arr.length-1-i; j++) {
+				if(arr[j]>arr[j+1]) {
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+					swapflag=true;
+				}
+			}
+			if(!swapflag)
+				break;
+		}
+	}
 	public static void main(String[] args) {
 		int []arr= {6,4,2,8,3,1};
 		System.out.println("Bubble sort");
 		System.out.println("Before"+Arrays.toString(arr));
-		bubbleSort(arr);
+		//bubbleSort(arr);
+		moreimpbubbleSort(arr);
 		System.out.println("After"+Arrays.toString(arr));
 		
 		System.out.println("Bubble sort");
