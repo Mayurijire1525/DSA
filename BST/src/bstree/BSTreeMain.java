4062f1240efe7696e778accd03e72767208b826d
Mayurijire1525 <Mayurijire441@gmail.com>
2024-06-24 14:11:00 +0530

    Daily codes

--- BST/src/bstree/BSTreeMain.java
+++ BST/src/bstree/BSTreeMain.java
@@ -1,5 +1,7 @@
 package bstree;
 
+import java.util.Stack;
+
 class BinarySearchTree {
 	static class Node {
 		// Node fields
@@ -55,7 +57,7 @@ class BinarySearchTree {
 		}
 	}
 
-	public void preorder(Node trav) {
+	private void preorder(Node trav) {
 		if (trav == null)
 			return;
 		System.out.print(trav.data + ", ");
@@ -69,7 +71,7 @@ class BinarySearchTree {
 		System.out.println();
 	}
 
-	public void inorder(Node trav) {
+	private void inorder(Node trav) {
 		if (trav == null)
 			return;
 		inorder(trav.left);
@@ -83,18 +85,74 @@ class BinarySearchTree {
 		System.out.println();
 	}
 
-	public void postorder(Node trav) {
+	private void postorder(Node trav) {
 		if (trav == null)
 			return;
 		postorder(trav.left);
 		postorder(trav.right);
 		System.out.print(trav.data + ", ");
 	}
-
+ 
 	public void postorder() {
-		System.out.print("POST :");
+		System.out.print(" POST :");
 		postorder(root);
 		System.out.println();
+	}
+	public void preorderNonRecursive() {
+		System.out.print ("PRE: ");
+		Stack<Node> s=new Stack<>();
+		Node trav=root;
+		while(trav!=null || !s.empty()) {
+			while(trav!=null) {
+				System.out.print(trav.data+", ");
+				if(trav.right!=null)
+					s.push(trav.right);
+				trav=trav.left;
+			}
+			if(!s.empty())
+				trav=s.pop();
+		}
+		System.out.println();
+	}
+	public void inorderNonRecursive() {
+		System.out.print ("IN: ");
+		Stack<Node> s=new Stack<>();
+		Node trav=root;
+		while(trav!=null || !s.empty()) {
+			while(trav!=null) {
+					s.push(trav);
+				trav=trav.left;
+			}
+			if(!s.empty())
+				trav=s.pop();
+			System.out.print(trav.data+", ");
+			trav=trav.right;
+		}
+		System.out.println();
+	}
+	private void deleteAll(Node trav) {
+		if(trav==null)
+			return;
+		deleteAll(trav.left);
+		deleteAll(trav.right);
+		trav.left=null;
+		trav.right=null;
+		trav=null;
+	}
+	public void deleteAll() {
+		deleteAll(root);
+		root=null;
+	}
+	public int height(Node trav) {
+		if(trav==null)
+			return -1;
+		int hl=height(trav.left);
+		int hr=height(trav.right);
+		int max=hl>hr?hl:hr;
+		return max+1;
+	}
+	public int height() {
+		return height(root);
 	}
 }
 
@@ -113,8 +171,14 @@ public class BSTreeMain {
 		t.add(60);
 		t.add(20);
 		t.preorder();
+		t.preorderNonRecursive(); 
 		t.inorder();
+		t.inorderNonRecursive();
 		t.postorder();
+		System.out.println("Height "+t.height());
 		// t.display();
+		t.deleteAll();
+		t.inorder();
+		System.out.println("Height "+t.height());
 	}
 }
