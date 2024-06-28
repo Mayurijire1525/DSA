05124d23dbc2c700c681143bc85a7311c5b24c3f
Mayurijire1525 <Mayurijire441@gmail.com>
2024-06-28 10:55:15 +0530

    Daily codes

--- BST/src/bstree/BST.java
+++ BST/src/bstree/BST.java
@@ -46,6 +46,34 @@ public class BST {
 		}
 		
 	}
+	public static Node delete(Node root,int val) {
+		if(root.data>val) {
+			root.left=delete(root.left,val);
+		}else if(root.data<val) {
+			root.right=delete(root.right,val);
+		}
+		else {
+			if(root.left==null && root.right==null) {
+				return null;
+			}
+		
+		if(root.left==null) {
+			return root.right;
+		}else if(root.right==null) {
+			return root.left;
+		}
+		Node IS=inorderSuccessor(root.right);
+		root.data=IS.data;
+		root.right=delete(root.right,IS.data);
+		}
+		return root;
+	}
+	public static Node inorderSuccessor(Node root) {
+		while(root.left!=null) {
+			root=root.left;
+		}
+		return root;
+	}
 	public static void main(String[] args) {
 		int values[]= {8,5,3,1,4,6,10,11,14};
 		Node root=null;
@@ -55,6 +83,8 @@ public class BST {
 		}
 		inorder(root);
 		System.out.println();
+		delete(root,4);
+		inorder(root);
 		
 		if(search(root,7)) {
 			System.out.println("Found");
