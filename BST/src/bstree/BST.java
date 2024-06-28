94636664c1f4b9e86a0855349de7b8ab5b05ef05
Mayurijire1525 <Mayurijire441@gmail.com>
2024-06-28 10:32:14 +0530

    Daily codes

--- BST/src/bstree/BST.java
+++ BST/src/bstree/BST.java
@@ -10,6 +10,7 @@ public class BST {
 			this.data=data;
 		}
 	}
+	
 	public static Node insert(Node root,int val) {
 		if(root==null) {
 			root=new Node(val);
@@ -26,14 +27,27 @@ public class BST {
 	}
 	public static void inorder(Node root) {
 		if(root==null) {
-			return;
+			 return;
 		}
 		inorder(root.left);
 		System.out.print(root.data+" ");
 		inorder(root.right);
 	}
+	public static boolean search(Node root,int key) {
+		if(root==null) {
+			return false;
+		}
+		if(root.data>key) {
+			return search(root.left,key);
+		}else if(root.data==key) {
+			return true;
+		}else{
+			return search(root.right,key);
+		}
+		
+	}
 	public static void main(String[] args) {
-		int values[]= {5,1,3,4,2,7};
+		int values[]= {8,5,3,1,4,6,10,11,14};
 		Node root=null;
 
 		for(int i=0; i<values.length; i++) {
@@ -41,6 +55,12 @@ public class BST {
 		}
 		inorder(root);
 		System.out.println();
+		
+		if(search(root,7)) {
+			System.out.println("Found");
+		}else {
+			System.out.println("Not found");
+		}
 	}
 
 }
